d672a85c7ec844b4ecc79f3ea5c93a14d1a978fe
Cecyyy <cecyparamo94@gmail.com>
2022-03-25 12:05:55 -0600

    creacion del main, de los do objetos, y los setters de ambos

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,35 @@
 public class Main {
+    public static void main(String[] args) {
+        Calculadora basica = new Calculadora("patito",false);
+        Calculadora cientifica = new Calculadora("casio",true);
+
+        basica.sumar();
+        basica.setPrimerValor(248.9);
+        basica.setSegundoValor(0.0);
+        basica.restar();
+        basica.setPrimerValor(248.9);
+        basica.setSegundoValor(0.0);
+        basica.multipricar();
+        basica.setPrimerValor(248.9);
+        basica.setSegundoValor(0.0);
+        basica.dividir();
+        basica.setPrimerValor(248.9);
+        basica.setSegundoValor(0.0);
+
+        cientifica.sumar();
+        cientifica.setPrimerValor(45.83);
+        cientifica.setSegundoValor(13.6);
+        cientifica.restar();
+        cientifica.setPrimerValor(45.83);
+        cientifica.setSegundoValor(13.6);
+        cientifica.multipricar();
+        cientifica.setPrimerValor(45.83);
+        cientifica.setSegundoValor(13.6);
+        cientifica.dividir();
+        cientifica.setPrimerValor(45.83);
+        cientifica.setSegundoValor(13.6);
+
+
+
+    }
 }
