0663a7879f22c8e18e074c70690905a215a42f53
Cecyyy <cecyparamo94@gmail.com>
2022-03-25 11:10:08 -0600

    creacion del constructor solo marca y booleano

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,2 +1,43 @@
 public class Calculadora {
+    private String marca;
+    private boolean esCientifica;
+    private double primerValor;
+    private double segundoValor;
+
+    public Calculadora(String marca, boolean esCientifica) {
+        this.marca = marca;
+        this.esCientifica = esCientifica;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public boolean isEsCientifica() {
+        return esCientifica;
+    }
+
+    public void setEsCientifica(boolean esCientifica) {
+        this.esCientifica = esCientifica;
+    }
+
+    public double getPrimerValor() {
+        return primerValor;
+    }
+
+    public void setPrimerValor(double primerValor) {
+        this.primerValor = primerValor;
+    }
+
+    public double getSegundoValor() {
+        return segundoValor;
+    }
+
+    public void setSegundoValor(double segundoValor) {
+        this.segundoValor = segundoValor;
+    }
 }
